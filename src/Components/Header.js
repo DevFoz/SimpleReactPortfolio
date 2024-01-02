4767ebbbad1d5b84b1c115960568983d3fd3c1d8
Faramoz Samim <124074099+DevFoz@users.noreply.github.com>
2024-01-02 14:56:07 -0500

    Commit

---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo">
+                <h1>Faramoz Samim</h1>
+            </div>
+            <nav className="navigation">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Coding Portfolio</a></li>
+                    <li><a href="/">Modelling Portfolio</a></li>
+                    <li><a href="/">Contact</a></li>
+                </ul>
+            </nav>
+            <div className="btn">
+                <button type="button">Login</button>
+            </div>
+        </div>
+    );
+}
+export default Header;
---
+++ src/Components/Main.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Main = () => {
+    return(
+        <div className="main">
+            <div className="col col1">
+                <h2>Software Developer</h2>
+                <p>Aspiring to be a Software Developer/Machine Learning Engineer<br />Extra Technologies I want to learn:</p>
+                <button type="button">More</button>
+            </div>
+            <div className="col">
+                <div className="card card1"></div>
+                <div className="card card2"></div>
+                <div className="card card3"></div>
+                <div className="card card4"></div>
+                <div className="card card5"></div>
+                <div className="card card6"></div>
+            </div>
+        </div>
+    );
+}
+export default Main;
